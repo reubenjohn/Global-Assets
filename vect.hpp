ed3513a2c368f7b76c9a1b3725ad36251825bef1
Reuben John <reubezzz1@gmail.com>
2014-02-12 09:38:22 +0300

    using template based vect + added random.hpp + other major changes

--- vect.hpp
+++ vect.hpp
@@ -13,11 +13,16 @@
 
 using namespace std;
 
+/**
+ * class that represents a mathematical vector quantity
+ * it also has various overloaded operators that allows for various procedures on it
+ */
+template<class T=long double>
 class vect
 {
 public:
-	long double x,y,z;
-	vect(long double user_x,long double user_y,long double user_z)
+	T x,y,z;
+	vect(T user_x,T user_y,T user_z)
 	{
 		x=user_x;
 		y=user_y;
@@ -27,7 +32,7 @@ public:
 	{
 		x=y=z=0;
 	}
-	virtual vect reverse()
+	vect reverse()
 	{
 		vect temp;
 		temp.x=-x;
@@ -35,22 +40,22 @@ public:
 		temp.z=-z;
 		return temp;
 	}
-	virtual vect operator=(vect b)
+	vect operator=(vect b)
 	{
 		x=b.x;
 		y=b.y;
 		z=b.z;
 		return *this;
 	}
-	virtual bool operator==(vect b)
+	bool operator==(vect b)
 	{
 		return (x==b.x&&y==b.y&&z==b.z);
 	}
-	virtual bool operator!=(vect b)
+	bool operator!=(vect b)
 	{
 		return (x!=b.x&&y!=b.y&&z!=b.z);
 	}
-	virtual vect operator+(vect b)
+	vect operator+(vect b)
 	{
 		vect temp;
 		temp.x=x+b.x;
@@ -58,7 +63,7 @@ public:
 		temp.z=z+b.z;
 		return temp;
 	}
-	virtual vect operator-(vect b)
+	vect operator-(vect b)
 	{
 		vect temp;
 		temp.x=x-b.x;
@@ -66,7 +71,7 @@ public:
 		temp.z=z-b.z;
 		return temp;
 	}
-	virtual vect operator*(vect b)
+	vect operator*(vect b)
 	{
 		vect temp;
 		temp.x=x*b.x;
@@ -74,7 +79,11 @@ public:
 		temp.z=z*b.z;
 		return temp;
 	}
-	virtual vect operator*(long double scalar)
+	vect operator/(vect b)
+	{
+		return (vect){x/b.x , y/b.y , z/b.z};
+	}
+	vect operator*(long double scalar)
 	{
 		vect temp;
 		temp.x=x*scalar;
@@ -82,7 +91,7 @@ public:
 		temp.z=z*scalar;
 		return temp;
 	}
-	virtual vect operator/(long double scalar)
+	vect operator/(long double scalar)
 	{
 		vect temp;
 		temp.x=x/scalar;
@@ -90,11 +99,11 @@ public:
 		temp.z=z/scalar;
 		return temp;
 	}
-	virtual long double operator|(vect b)
+	T operator|(vect b)
 	{
 		return x*b.x+y*b.y+z*b.z;
 	}
-	virtual vect operator^(vect b)
+	vect operator^(vect b)
 	{
 		vect temp;
 		temp.x=y*b.z-z*b.y;
@@ -102,244 +111,89 @@ public:
 		temp.z=x*b.y-b.x*y;
 		return temp;
 	}
-	virtual vect operator+=(vect b)
+	vect operator+=(vect b)
 	{
 		x+=b.x;
 		y+=b.y;
 		z+=b.z;
 		return *this;
 	}
-	virtual vect operator-=(vect b)
+	vect operator-=(vect b)
 	{
 		x-=b.x;
 		y-=b.y;
 		z-=b.z;
 		return *this;
 	}
-	virtual vect operator*=(vect b)
+	vect operator*=(vect b)
 	{
 		x*=b.x;
 		y*=b.y;
 		z*=b.z;
 		return *this;
 	}
-	virtual vect operator*=(long double scalar)
+	vect operator*=(long double scalar)
 	{
 		x*=scalar;
 		y*=scalar;
 		z*=scalar;
 		return *this;
 	}
-	virtual vect operator/=(long double scalar)
+	vect operator/=(long double scalar)
 	{
 		x/=scalar;
 		y/=scalar;
 		z/=scalar;
 		return *this;
 	}
-	virtual long double mag()
+	T mag()
 	{
 		return sqrt(x*x+y*y+z*z);
 	}
-	virtual long double separation(vect U)
+	T separation(vect U)
 	{
 		vect temp(*this-U);
 		return sqrt(temp.x*temp.x+temp.y*temp.y+temp.z*temp.z);
 	}
-	virtual vect dir()
+	vect dir()
 	{
 		vect temp;
 		temp.x=x;
 		temp.y=y;
 		temp.z=z;
-		return temp/temp.mag();
+		T mag=temp.mag();
+		if(mag)
+			return temp/temp.mag();
+		else
+			return (vect){0,0,0};
 	}
-	virtual vect reset()
+	vect reset()
 	{
 		x=y=z=0.0;
 		return *this;
 	}
-	virtual ~vect()
-	{
-
-	}
 };
-vect operator*(long double scalar,vect b)
+template<class T>
+vect<T> operator*(long double scalar,vect<T> b)	//overloading * operator to allow multiplication of a scalar with a vector
 {
-       vect temp;
+       vect<T> temp;
        temp.x=b.x*scalar;
        temp.y=b.y*scalar;
        temp.z=b.z*scalar;
        return temp;
 }
-void operator<<(ofstream &fout,vect a)
+template<class T>
+void operator<<(ofstream &fout,vect<T> a)	//overloading if << operator to allo displaying of a vector onto a stream
 {
 	fout<<"("<<a.x<<","<<a.y<<","<<a.z<<")";
 }
-vect operator-(vect b)
+template<class T>
+vect<T> operator-(vect<T> b)	//overloading of - unary operator to allow a vector to reversed
 {
-	vect temp;
+	vect<T> temp;
 	temp.x=-b.x;
 	temp.y=-b.y;
 	temp.z=-b.y;
 	return temp;
 }
-long double mag(vect b)
-{
-	return sqrt((b.x*b.x+b.y*b.y+b.z*b.z));
-}
-/*class unsigned_int_vect:public vect
-{
-public:
-	unsigned int x,y,z;
-	unsigned_int_vect(unsigned int user_x,unsigned int user_y,unsigned int user_z)
-	{
-		x=user_x;
-		y=user_y;
-		z=user_z;
-	}
-	unsigned_int_vect()
-	{
-		x=y=z=0;
-	}
-	virtual unsigned_int_vect reverse_unsigned_int_vect()
-	{
-		unsigned_int_vect temp;
-		temp.x=-x;
-		temp.y=-y;
-		temp.z=-z;
-		return temp;
-	}
-	virtual unsigned_int_vect operator=(unsigned_int_vect b)
-	{
-		x=b.x;
-		y=b.y;
-		z=b.z;
-		return *this;
-	}
-	virtual bool operator==(unsigned_int_vect b)
-	{
-		return (x==b.x&&y==b.y&&z==b.z);
-	}
-	virtual bool operator!=(unsigned_int_vect b)
-	{
-		return (x!=b.x&&y!=b.y&&z!=b.z);
-	}
-	virtual unsigned_int_vect operator+(unsigned_int_vect b)
-	{
-		unsigned_int_vect temp;
-		temp.x=x+b.x;
-		temp.y=y+b.y;
-		temp.z=z+b.z;
-		return temp;
-	}
-	virtual unsigned_int_vect operator-(unsigned_int_vect b)
-	{
-		unsigned_int_vect temp;
-		temp.x=x-b.x;
-		temp.y=y-b.y;
-		temp.z=z-b.z;
-		return temp;
-	}
-	virtual unsigned_int_vect operator*(unsigned_int_vect b)
-	{
-		unsigned_int_vect temp;
-		temp.x=x*b.x;
-		temp.y=y*b.y;
-		temp.z=z*b.z;
-		return temp;
-	}
-	virtual unsigned_int_vect operator*(unsigned int scalar)
-	{
-		unsigned_int_vect temp;
-		temp.x=x*scalar;
-		temp.y=y*scalar;
-		temp.z=z*scalar;
-		return temp;
-	}
-	virtual unsigned_int_vect operator/(unsigned int scalar)
-	{
-		unsigned_int_vect temp;
-		temp.x=x/scalar;
-		temp.y=y/scalar;
-		temp.z=z/scalar;
-		return temp;
-	}
-	virtual unsigned int operator|(unsigned_int_vect b)
-	{
-		return x*b.x+y*b.y+z*b.z;
-	}
-	virtual unsigned_int_vect operator^(unsigned_int_vect b)
-	{
-		unsigned_int_vect temp;
-		temp.x=y*b.z-z*b.y;
-		temp.y=-(x*b.z-b.x*z);
-		temp.z=x*b.y-b.x*y;
-		return temp;
-	}
-	virtual unsigned_int_vect operator+=(unsigned_int_vect b)
-	{
-		x+=b.x;
-		y+=b.y;
-		z+=b.z;
-		return *this;
-	}
-	virtual unsigned_int_vect operator-=(unsigned_int_vect b)
-	{
-		x-=b.x;
-		y-=b.y;
-		z-=b.z;
-		return *this;
-	}
-	virtual unsigned_int_vect operator*=(unsigned_int_vect b)
-	{
-		x*=b.x;
-		y*=b.y;
-		z*=b.z;
-		return *this;
-	}
-	virtual unsigned_int_vect operator*=(unsigned int scalar)
-	{
-		x*=scalar;
-		y*=scalar;
-		z*=scalar;
-		return *this;
-	}
-	virtual unsigned_int_vect operator/=(unsigned int scalar)
-	{
-		x/=scalar;
-		y/=scalar;
-		z/=scalar;
-		return *this;
-	}
-	virtual long double mag()
-	{
-		return sqrt(x*x+y*y+z*z);
-	}
-	virtual unsigned int separation(unsigned_int_vect U)
-	{
-		unsigned_int_vect temp(*this-U);
-		return sqrt(temp.x*temp.x+temp.y*temp.y+temp.z*temp.z);
-	}
-	virtual vect dir()
-	{
-		vect temp;
-		temp.x=x;
-		temp.y=y;
-		temp.z=z;
-		return temp/temp.mag();
-	}
-	virtual vect reset()
-	{
-		vect temp;
-		temp.x=x;
-		temp.y=y;
-		temp.z=z;
-		return temp/temp.mag();
-	}
-	~unsigned_int_vect()
-	{
-
-	}
-};*/
 #endif /* VECT_HPP_ */
